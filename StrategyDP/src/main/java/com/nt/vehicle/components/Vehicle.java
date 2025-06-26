e3173fff66c8f3fc560e0fe6f99bb5a2bffc716c
Prashant Gaware <prashantgaware8@gmail.com>
2025-06-26 22:29:40 +0530

    strategy design pattern : vehicle example

--- StrategyDP/src/main/java/com/nt/vehicle/components/Vehicle.java
+++ StrategyDP/src/main/java/com/nt/vehicle/components/Vehicle.java
@@ -12,7 +12,7 @@ public class Vehicle {
     }
 
 
-    public void drive() {
-        driveStrategy.drive();
+    public String drive() {
+        return driveStrategy.drive();
     }
 }
--- StrategyDP/src/main/java/com/nt/vehicle/test/VehicleTest.java
+++ StrategyDP/src/main/java/com/nt/vehicle/test/VehicleTest.java
@@ -9,17 +9,17 @@ public class VehicleTest {
         // Create a vehicle with normal driving strategy
         Vehicle vehicle1 = new Vehicle(new NormalDrive());
         System.out.println("Vehicle 1 is ready with Normal Drive strategy.");
-        vehicle1.drive();
+        System.out.println(vehicle1.drive());
         System.out.println();
         // Create a vehicle with off-road driving strategy
         Vehicle vehicle2 = new Vehicle(new OffRoadDrive());
         System.out.println("Vehicle 2 is ready with Off-Road Drive strategy.");
-        vehicle2.drive();
+        System.out.println(vehicle2.drive());
         System.out.println();
         // Change the driving strategy of vehicle1 to off-road
         System.out.println("Changing Vehicle 1's driving strategy to Off-Road Drive.");
         vehicle1 = new Vehicle(new OffRoadDrive());
         System.out.println("Vehicle 1 is now ready with Off-Road Drive strategy.");
-        vehicle1.drive();
+        System.out.println(vehicle1.drive());
     }
 }
