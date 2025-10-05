8f10141a4842f3259f8dfc141e0dfc904631c387
Prashant Gaware <prashantgaware8@gmail.com>
2025-10-05 20:54:03 +0530

    null object dp

--- NullObjectDP/src/dp/logger/LoggerClient.java
+++ NullObjectDP/src/dp/logger/LoggerClient.java
@@ -4,8 +4,9 @@ public class LoggerClient {
 
     public static void main(String[] args) {
         // Using NullLogger
-        Logger nullLogger = LoggerFactory.getLogger("NULL");
+        Logger nullLogger = LoggerFactory.getLogger(null);
         nullLogger.log("This log message will be ignored.");
+
         // Using RealLogger
         Logger logger = LoggerFactory.getLogger("REAL");
         logger.log("This is a real log message.");
